21f7d3512be24144850322a5890c499647f59a5d
Warren Sangster <warren@moneyspyder.co.uk>
2014-01-13 21:12:44 +0000

    Error with sphinx - unexpected end

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -5,7 +5,7 @@ execute "testing" do
 end
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
-require_recipe "sphinx"
+# require_recipe "sphinx"
 
 # uncomment to use the collectd recipe. See cookbooks/collectd/readme.md for documentation.
 # include_recipe "collectd"
