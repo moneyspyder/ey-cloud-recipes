defb3028085a3779fc9cfab147e0ae8dd60f9f45
Warren Sangster <warren@moneyspyder.co.uk>
2014-10-17 17:16:34 +0100

    Trying custom packages

--- cookbooks/packages/attributes/packages.rb
+++ cookbooks/packages/attributes/packages.rb
@@ -7,4 +7,7 @@
 # Examples below:
 
 default[:packages] = [{:name => "app-misc/wkhtmltopdf-bin", :version => "0.10.0_beta5"},
-                       {:name => "dev-util/lockrun", :version => "2-r1"}]
+                       #{:name => "dev-util/lockrun", :version => "2-r1"},
+                       {:name => "net-misc/openssh", :version => "~6.1_p1"}
+                     ]
+#5.9_p1-r5
